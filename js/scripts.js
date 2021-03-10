b3dc23b6814f2059cbaeb7c126d6b9f26059b272
TomasMoratoPerezPorro <49519581+TomasMoratoPerezPorro@users.noreply.github.com>
2021-03-10 19:40:58 +0100

    Hero photo, menu and carroussel components

---
+++ js/scripts.js
@@ -0,0 +1,203 @@
+'use strict';
+const buttons = document.querySelectorAll('.button');
+
+$(document).ready(function () {
+  listaPicaPica();
+  apartatCarta();
+  carrousel();
+  /* galeria(); */
+});
+
+function transparent() {
+  $('.topbar').removeClass('topbar-white').addClass('topbar-transparent');
+}
+function white() {
+  $('.topbar').removeClass('topbar-transparent').addClass('topbar-white');
+}
+
+function scrollMenu() {
+  5 < $(window).scrollTop() ? white() : transparent();
+}
+scrollMenu(),
+  $(window).on({
+    scroll: function () {
+      scrollMenu();
+    },
+  });
+
+window.onscroll = function () {
+  myFunction();
+  scrollMenu();
+};
+
+var header = document.getElementById('myHeader');
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+}
+
+function carrousel() {
+  var e = $('.carousel');
+  e.length &&
+    e.each(function () {
+      $(this).flickity({
+        cellAlign: 'left',
+        contain: true,
+      });
+    });
+}
+
+function apartatCarta() {
+  $('.mybutton').click(function () {
+    //alert("The paragraph was clicked.");
+    buttons.forEach((button) => button.classList.remove('active'));
+    this.classList.add('active');
+    if ($(this).attr('id') == 'picapica') {
+      console.log('this is picapica');
+      listaPicaPica();
+    } else if ($(this).attr('id') == 'primers') {
+      console.log('this is primers');
+      listaPrimers();
+    } else if ($(this).attr('id') == 'segons') {
+      console.log('this is segons');
+      listaSegons();
+    } else {
+      console.log('this is postre');
+      listaPostres();
+    }
+  });
+}
+
+function listaPicaPica() {
+  $.ajax({
+    url: 'carta/carta.json',
+    type: 'get',
+    datatype: 'json',
+    beforeSend: function () {
+      $('#contenidos').html('Procesando, espere por favor...');
+    },
+    success: function (data) {
+      //var resposta=JSON.parse(data);
+      console.log(data);
+      var resposta = data;
+      console.log(resposta);
+      $('#contenidos').empty();
+      $.each(resposta.picapica, function (index, element) {
+        $('#contenidos').append(
+          '<article class="item"><div class="item__header"><h3 class="item__title">' +
+            element.titol +
+            '</h3><span class="item__dots"></span><span class="item__price"></span></div></article>'
+        );
+        //$("#contenidos").append('<p>'+element.titol+'</p>');
+      });
+    },
+    error: function (e) {
+      //called when there is an error
+      console.log(e.message);
+      $('#contenidos').html('Ha habido un error ' + e.message);
+    },
+  });
+}
+
+function listaPrimers() {
+  $.ajax({
+    url: 'carta/carta.json',
+    type: 'get',
+    datatype: 'json',
+    beforeSend: function () {
+      $('#contenidos').html('Procesando, espere por favor...');
+    },
+    success: function (data) {
+      //var resposta=JSON.parse(data);
+      console.log(data);
+      var resposta = data;
+      console.log(resposta);
+      $('#contenidos').empty();
+      $.each(resposta.primers, function (index, element) {
+        $('#contenidos').append(
+          '<div class="item"><div class="item__header"><h3 class="item__title">' +
+            element.titol +
+            '</h3><span class="item__dots"></span><span class="item__price"></span></div><p class="item__description">' +
+            element.descripcio +
+            '</p></div>'
+        );
+        //$("#contenidos").append('<p>'+element.titol+'</p>');
+      });
+    },
+    error: function (e) {
+      //called when there is an error
+      console.log(e.message);
+      $('#contenidos').html('Ha habido un error ' + e.message);
+    },
+  });
+}
+
+function listaSegons() {
+  $.ajax({
+    url: 'carta/carta.json',
+    type: 'get',
+    datatype: 'json',
+    beforeSend: function () {
+      $('#contenidos').html('Procesando, espere por favor...');
+    },
+    success: function (data) {
+      //var resposta=JSON.parse(data);
+      console.log(data);
+      var resposta = data;
+      console.log(resposta);
+      $('#contenidos').empty();
+      $.each(resposta.segons, function (index, element) {
+        $('#contenidos').append(
+          '<div class="item"><div class="item__header"><h3 class="item__title">' +
+            element.titol +
+            '</h3><span class="item__dots"></span><span class="item__price"></span></div><p class="item__description">' +
+            element.descripcio +
+            '</p></div>'
+        );
+        //$("#contenidos").append('<p>'+element.titol+'</p>');
+      });
+    },
+    error: function (e) {
+      //called when there is an error
+      console.log(e.message);
+      $('#contenidos').html('Ha habido un error ' + e.message);
+    },
+  });
+}
+
+function listaPostres() {
+  $.ajax({
+    url: 'carta/carta.json',
+    type: 'get',
+    datatype: 'json',
+    beforeSend: function () {
+      $('#contenidos').html('Procesando, espere por favor...');
+    },
+    success: function (data) {
+      //var resposta=JSON.parse(data);
+      console.log(data);
+      var resposta = data;
+      console.log(resposta);
+      $('#contenidos').empty();
+      $.each(resposta.postres, function (index, element) {
+        $('#contenidos').append(
+          '<div class="item"><div class="item__header"><h3 class="item__title">' +
+            element.titol +
+            '</h3><span class="item__dots"></span><span class="item__price"></span></div></div>'
+        );
+        //$("#contenidos").append('<p>'+element.titol+'</p>');
+      });
+    },
+    error: function (e) {
+      //called when there is an error
+      console.log(e.message);
+      $('#contenidos').html('Ha habido un error ' + e.message);
+    },
+  });
+}
